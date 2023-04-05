6bf11326da363c8330435a0f51702c4f703a31f5
bellbind <bellbind@gmail.com>
2023-04-05 21:04:19 +0900

    add a test case of different timing of key added for remote event following test

--- test/many-coops/follow-via-intermediate-test.js
+++ test/many-coops/follow-via-intermediate-test.js
@@ -40,8 +40,8 @@ describe("coop follow with remote event via a intermediate node", async () => {
     });
     // connect node1 and node2 and node3
     await node2.swarm.connect((await node1.id()).addresses[0]);
-    await node3.swarm.connect((await node2.id()).addresses[0]);
-    await node1.swarm.connect((await node3.id()).addresses[0]); // instead of using peer discovery
+    await node3.swarm.connect((await node1.id()).addresses[0]);
+    await node3.swarm.connect((await node2.id()).addresses[0]); // instead of using peer discovery
     
     http2p1 = await createHttp2p(node1.libp2p);
     http2p2 = await createHttp2p(node2.libp2p);
@@ -60,12 +60,12 @@ describe("coop follow with remote event via a intermediate node", async () => {
   });
 
 
-  it("follow via intermediary", async () => {
+  it("follow via intermediary (add akey at one side)", async () => {
     const coop1 = createCoop(http2p1);
     const coop2 = createCoop(http2p2);
     const coop3 = createCoop(http2p3);
     coop1.keys.add("coop1");
-    coop1.keys.add("coop3");
+    coop1.keys.add("coop3"); // key:coop3 already existed at coop1
     coop2.keys.add("coop1");
     coop2.keys.add("coop2");
     coop3.keys.add("coop2");
@@ -89,6 +89,55 @@ describe("coop follow with remote event via a intermediate node", async () => {
     //console.log("coop1", coop1.uri);
     //console.log("coop2", coop2.uri);
     //console.log("coop3", coop3.uri);
+    coop3.keys.add("coop3");
+    {
+      const waitCoop1Followings = checkCoopDetected(coop1, 1);
+      const waitCoop3Followings = checkCoopDetected(coop3, 1);
+      await Promise.all([waitCoop1Followings, waitCoop3Followings]);
+      const coop1Followings = coop1.followings.followings();
+      const coop2Followings = coop2.followings.followings();
+      const coop3Followings = coop3.followings.followings();
+      assert.equal(coop1Followings.length, 2);
+      assert.equal(coop2Followings.length, 2);
+      assert.equal(coop3Followings.length, 2);      
+    }
+    
+    coop1.stop();
+    coop2.stop();
+    coop3.stop();
+  });
+
+  it("follow via intermediary (add a key both side)", async () => {
+    const coop1 = createCoop(http2p1);
+    const coop2 = createCoop(http2p2);
+    const coop3 = createCoop(http2p3);
+    coop1.keys.add("coop1");
+    coop2.keys.add("coop1");
+    coop2.keys.add("coop2");
+    coop3.keys.add("coop2");
+    
+    {
+      const waitCoop1Follows = checkCoopDetected(coop1, 1);
+      const waitCoop2Follows = checkCoopDetected(coop2, 2);
+      const waitCoop3Follows = checkCoopDetected(coop3, 1);
+      const res1 = await coop2.http2p.fetch(coop1.uri);
+      const res2 = await coop3.http2p.fetch(coop2.uri);
+      await Promise.all([waitCoop1Follows, waitCoop2Follows, waitCoop3Follows]);
+
+      const coop1Followings = coop1.followings.followings();
+      const coop2Followings = coop2.followings.followings();
+      const coop3Followings = coop3.followings.followings();
+      assert.equal(coop1Followings.length, 1);
+      assert.equal(coop2Followings.length, 2);
+      assert.equal(coop3Followings.length, 1);
+      //console.log("pass");
+    }
+    //console.log("coop1", coop1.uri);
+    //console.log("coop2", coop2.uri);
+    //console.log("coop3", coop3.uri);
+
+    // add key:coop3 into both coop1 and coop3
+    coop1.keys.add("coop3"); 
     coop3.keys.add("coop3");
     {
       const waitCoop1Followings = checkCoopDetected(coop1, 1);

da33c11b09a95030ea540a03b263d0601eb602ae
bellbind <bellbind@gmail.com>
2023-07-03 18:22:32 +0900

    update browser to nodejs tests

--- test-for-browser/helia-wrtcstar/creation-test.mjs
+++ test-for-browser/helia-wrtcstar/creation-test.mjs
@@ -4,15 +4,12 @@ import {strict as assert} from "node:assert";
 import {createServer} from "http-server";
 import {chromium} from "playwright";
 
-import {multiaddr} from "@multiformats/multiaddr";
-
-
 import {createServers} from "../../create-gateway-servers.js";
 import {createHeliaWithWrtcstar, bootstrapConfig} from "../common/helia-wrtcstar.js";
 
-describe("helia-wrtcstar", async () => {
+
+describe("helia-wrtcstar:creation", async () => {
   let httpServer, gatewayServers, browser;
-  let page, node, addrs;
   before(async () => {
     httpServer = createServer();
     await new Promise(f => httpServer.server.listen(8000, f));
@@ -22,25 +19,41 @@ describe("helia-wrtcstar", async () => {
       gateway: {port: 9000},
     });
     browser = await chromium.launch();
-
-    // nodejs helia
+  });
+  after(async () => {
+    await browser.close();
+    await new Promise(f => httpServer.server.close(f));
+    await gatewayServers.stop();
+  });
+  
+  it("connect helia nodes from browser to nodejs", async () => {
     const sigAddrs = gatewayServers.info.sig;
     // helia node on nodejs
-    node = await createHeliaWithWrtcstar(sigAddrs);
+    const node = await createHeliaWithWrtcstar(sigAddrs);
+    // helia with echo back
+    const proto = "/my-echo/0.1";
+    const handler = ({connection, stream}) => {
+      stream.sink(async function* () {
+        for await (const bufs of stream.source) {
+          yield bufs.slice().slice();
+        }
+      }());
+    };
+    await node.libp2p.handle(proto, handler);
+    const starAddr = node.libp2p.getMultiaddrs().find(ma => `${ma}`.includes("/p2p-webrtc-star/"));
+    const nodeAddr = `${starAddr}`;
     
-    // browser helia
-    page = await browser.newPage();
+    // run codes on browser page
+    const page = await browser.newPage();
     // load empty page includes only importmap
     await page.goto("http://localhost:8000/test-for-browser/common/index.html");
     page.on("console", msg => {
       if (msg.type() === "log") console.log(msg.location(), msg.text());
     });
     // evaluete test codes run on browser 
-    addrs = await page.evaluate(({bootstrapConfig, sigAddrs}) => (async () => {
+    const r = await page.evaluate(({bootstrapConfig, sigAddrs, proto, nodeAddr}) => (async () => {
       // import() in async (replacement from import statement)
       const {createHelia} = await import("helia");
-      const {unixfs} = await import("@helia/unixfs");
-      const {CID} = await import("multiformats/cid");
       const {multiaddr} = await import("@multiformats/multiaddr");
       const {bootstrap} = await import("@libp2p/bootstrap");
       const {circuitRelayTransport} = await import("libp2p/circuit-relay");
@@ -74,49 +87,14 @@ describe("helia-wrtcstar", async () => {
           connectionGater: {denyDialMultiaddr: async (...args) => false},
         },
       }); // tcp network, stored on memory (not use files)
+
       // wait to connect
       while (node.libp2p.getMultiaddrs().length === 0) await new Promise(f => setTimeout(f, 500));
-      
-      const nodefs = unixfs(node);
-      globalThis.ctx = {multiaddr, CID, node, nodefs};
-      
-      return {
-        peerId: node.libp2p.peerId.toString(),
-        multiaddr: node.libp2p.getMultiaddrs()[0].toString(),
-      };
-    })(), {bootstrapConfig, sigAddrs});
-    //console.log(addrs);
-  });
-  after(async () => {
-    await page.evaluate(() => (async () => {
-      await ctx.node.stop();
-    })());
-    await node.stop();
-    
-    await browser.close();
-    await new Promise(f => httpServer.server.close(f));
-    await gatewayServers.stop();
-  });
-  
-  it("dial from browser to nodejs", async () => {
-    const proto = "/my-echo/0.1";
-    // handle on nodejs
-    const handler = ({connection, stream}) => {
-      stream.sink(async function* () {
-        for await (const bufs of stream.source) {
-          yield bufs.slice().slice();
-        }
-      }());
-    };
-    await node.libp2p.handle(proto, handler);
-    const starAddr = node.libp2p.getMultiaddrs().find(ma => `${ma}`.includes("/p2p-webrtc-star/"));
-    const nodeAddr = `${starAddr}`;
-    
-    // dial on browser page
-    const r = await page.evaluate(({proto, nodeAddr}) => (async () => {
+
+      // dialProtocol to nodejs helia
       const send = async (ma, msg) => {
-        if (typeof ma === "string") ma = ctx.multiaddr(ma);
-        const stream = await ctx.node.libp2p.dialProtocol(ma, proto);
+        if (typeof ma === "string") ma = multiaddr(ma);
+        const stream = await node.libp2p.dialProtocol(ma, proto);
         stream.sink(async function* () {
           yield (new TextEncoder().encode(msg));
         }());
@@ -124,49 +102,17 @@ describe("helia-wrtcstar", async () => {
           return new TextDecoder().decode(bufs.slice().slice());
         }
       };
-      return await send(nodeAddr, "Hello from browser");
-    })(), {proto, nodeAddr});
+      const ret = await send(nodeAddr, "Hello from browser");
 
-    // check results
+      // clean up and return on page
+      await node.stop();
+      return ret;
+    })(), {bootstrapConfig, sigAddrs, proto, nodeAddr});
+
+    // check results on test
     assert.equal(r, "Hello from browser");
-    
-    // cleanup on nodejs
-    await node.libp2p.unhandle(proto);
-  });
 
-  it("dial from nodejs to browser", async () => {
-    const proto = "/my-echo/0.1";
-    // handle on browser
-    await page.evaluate(({proto}) => (async () => {
-      const handler = ({connection, stream}) => {
-        stream.sink(async function* () {
-          for await (const bufs of stream.source) {
-            yield bufs.slice().slice();
-          }
-        }());
-      };
-      await ctx.node.libp2p.handle(proto, handler);
-    })(), {proto});
-
-    // dial on nodejs
-    const send = async (ma, msg) => {
-      if (typeof ma === "string") ma = multiaddr(ma);
-      const stream = await node.libp2p.dialProtocol(ma, proto);
-      stream.sink(async function* () {
-        yield (new TextEncoder().encode(msg));
-      }());
-      for await (const bufs of stream.source) {
-        return new TextDecoder().decode(bufs.slice().slice());
-      }
-    };
-    const r = await send(addrs.multiaddr, "Hello from nodejs");
-    
-    // check results
-    assert.equal(r, "Hello from nodejs");
-
-    // cleanup on browser
-    await page.evaluate(({proto}) => (async () => {
-      await ctx.node.libp2p.unhandle(proto);
-    })(), {proto});
+    // clean up on nodejs
+    await node.stop();
   });
 });
